8023cee41074bc42feb00f710248384519f36a27
Vladislav <vladislav-bodnya@mail.ru>
2025-04-27 00:52:37 +0300

    Deleted unneccessary files

--- New/monitor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Client
-
-@admin.register(Client)
-class ClientAdmin(admin.ModelAdmin):
-    list_display = ('domain', 'computer_name', 'ip_address', 'username', 'last_active')
